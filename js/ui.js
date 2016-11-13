d605dc9f91e64ebc623bc4b144d2c067e8d2d1c1
Radovan Ristovic <radovanr995@gmail.com>
2016-11-14 00:39:27 +0100

    js kod pomeren u ui.js, sve stranice sada imaju isti header

---
+++ js/ui.js
@@ -0,0 +1,30 @@
+var gogColor = document.getElementById('h1-GOG').style.color;
+var gogNewColor = "red";
+var aGog = document.getElementById('a-GOG');
+
+aGog.onmouseover = function(){document.getElementById('h1-GOG').style.color = gogNewColor}
+aGog.onmouseout = function(){document.getElementById('h1-GOG').style.color = gogColor}
+
+var menuItemBG;
+$(document).ready(function(){
+  menuItemBG = $('.navbar.navbar-inverse').css('background');
+}); 
+$('#navbar a').hover(function(){
+  $(this).css('background','#151E8C');
+  },
+  function(){
+    $(this).css('background', menuItemBG);
+  }
+);
+
+function initMap() {
+        var uluru = {lat: 44.791525, lng: 20.489902};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
