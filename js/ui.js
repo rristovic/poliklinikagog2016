e1e9eb4c82e5ed9035d68eaa40cbb871c905a146
David Vukasinovic <davidvukasinovic@yahoo.com.au>
2016-11-14 17:52:06 +0100

    Dodat image slider i footer

--- js/ui.js
+++ js/ui.js
@@ -43,3 +43,22 @@ function initMap() {
 }
 
 w3IncludeHTML();
+
+function slider() {
+  $(".slider #1").show("fade", 500);
+  $(".slider #1").delay(5500).hide("slide",{direction: 'left'}, 500);
+
+  var sc = $(".slider img").size();
+  var count=2;
+
+  setInterval(function(){
+    $(".slider #"+count).show("fade",{direction: 'right'}, 500);
+    $(".slider #"+count).delay(5500).hide("slide",{direction: 'left'}, 500);
+
+    if(count==sc){
+      count=1;
+    }else{
+      count=count+1;
+    }
+  }, 6500);
+}
